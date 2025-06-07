7e628d997fecf8452159d8f0998bc6a8b04cf871
vasumakhija <140094234+vasumakhija@users.noreply.github.com>
2025-06-08 01:46:10 +0530

    Update 2.py

--- 2.py
+++ 2.py
@@ -8,7 +8,7 @@ from fpdf import FPDF
 from googletrans import Translator
 
 # === CONFIG ===
-GROQ_API_KEY = os.getenv("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")
+GROQ_API_KEY = os.getenv("your_Grok_Api")
 MODEL = "llama3-8b-8192"
 SAVE_FILE = "candidates.json"
 
